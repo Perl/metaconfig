da978234f9ba6bb6237ca964b8c305b2ba783da3
H.Merijn Brand <h.m.brand@xs4all.nl>
2006-11-16 08:44:09 +0000

    As generation of Glossary depends on a copy of config.sh, make sure it is up to date enough to make a reliable Glossary.
    p4raw-id: //depot/metaconfig@29290

--- U/mkgloss.pl
+++ U/mkgloss.pl
@@ -1,12 +1,31 @@
 #!/pro/bin/perl
 
-# (c)'06 H.Merijn Brand [ 09 Apr 2006 ]
+# (c)'06 H.Merijn Brand [ 16 Nov 2006 ]
 
 # This script combines the output of mkglossary, with the suggested
 # patches (see README), and sorts the list
 
 use strict;
 use warnings;
+
+my $Pcsh = "Porting/config.sh";
+@ARGV = "config.sh";
+my @config = <> or die
+    "mkgloss uses $Pcsh.sh. You didn't run Configure (yet)\n".
+    "so I cannot make a temporary version of it to generate a reliable\n".
+    "Glossary!\n";
+{   my $drop = 0;
+    for (@config) {
+	s{\bh.m.brand\@\w+\.nl\b}{hmbrand\@cpan.org};
+	s{\b\.xs4all.nl\b}{.cpan.org};
+	m/^# Variables propagated from previous config/ and $drop++;
+	$drop and $_ = '';
+	}
+    unlink $Pcsh;
+    open my $config, ">", $Pcsh or die "$Pcsh: $!\n";
+    print   $config @config;
+    close   $config;
+    }
 
 @ARGV = ("U/mkglossary |");
 $/ = undef;
@@ -28,5 +47,6 @@ for (sort { lc $a cmp lc $b } @g) {
     if (m/^spitshell /) {
 	s/cat or a grep -v for # comments/cat or a grep '-v' for # comments/;
 	}
+    s/[ \t]+\n/\n/g;
     print;
     }
