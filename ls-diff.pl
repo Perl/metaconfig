c666780405c195cfdc93ae5132c34e7395532850
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2016-04-22 18:09:38 +0200

    git => dist
    These are the simple copies with no changes but the RCS headers

--- ls-diff.pl
+++ ls-diff.pl
@@ -20,6 +20,7 @@ GetOptions (
 
     "l|list!"	=> \my $opt_l,
     "d|diff=s"	=> \my $opt_d,
+    "c|copy"	=> \my $opt_c,
     ) or usage (1);
 
 my $pat = shift // ".";
@@ -38,7 +39,12 @@ foreach my $u ( [ "d", "dist/U"          ],
 	m{$pat} or  return;
 
 	my $u = do { local (@ARGV, $/) = $_; <> };
-	$m{$_}{$t} = [ $File::Find::dir, $u, length ($u), ($u =~ tr/\n/\n/) ];
+	$m{$_}{$t} = {
+	    dir   => $File::Find::dir,
+	    unit  => $u,
+	    size  => length ($u),
+	    lines => ($u =~ tr/\n/\n/),
+	    };
 	}, $dir);
     }
 
@@ -52,14 +58,50 @@ foreach my $u (sort keys %m) {
     my $m = $m{$u}{m} or next;
     my $g = $m{$u}{g} or next;
 
-    printf "%3d %5d/%3d %6d %5d/%3d %6d %5d/%3d %6d %s\n", $i++,
-	$g->[2], $g->[3], length (diff (\$g->[1], \$d->[1])),
-	$d->[2], $d->[3], length (diff (\$d->[1], \$m->[1])),
-	$m->[2], $m->[3], length (diff (\$g->[1], \$m->[1])),
-	$u;
-    $opt_l and say "    $m{$u}{$_}[0]/$u" for qw( g d m );
+    my $gd = length diff (\$g->{unit}, \$d->{unit});
+    my $dm = length diff (\$d->{unit}, \$m->{unit});
+    my $gm = length diff (\$g->{unit}, \$m->{unit});
 
-    $opt_d eq "dm" and system "diff", "-w", "$d->[0]/$u", "$m->[0]/$u";
-    $opt_d eq "dg" and system "diff", "-w", "$d->[0]/$u", "$g->[0]/$u";
-    $opt_d eq "gm" and system "diff", "-w", "$g->[0]/$u", "$m->[0]/$u";
+    printf "%3d %5d/%3d %6d %5d/%3d %6d %5d/%3d %6d %s\n", $i++,
+	$g->{size}, $g->{lines}, $gd,
+	$d->{size}, $d->{lines}, $dm,
+	$m->{size}, $m->{lines}, $gm,
+	$u;
+    $opt_l and say "    $m{$u}{$_}{dir}/$u" for qw( g d m );
+
+    extdiff ($u, sort split // => $opt_d);
+
+    $opt_d eq "dg" and system "diff", "-w", "$d->{dir}/$u", "$g->{dir}/$u";
+    $opt_d eq "gm" and system "diff", "-w", "$g->{dir}/$u", "$m->{dir}/$u";
     }
+
+sub extdiff {
+    my ($u, $from, $to) = (@_, "", "");
+
+    my $f = $m{$u}{$from} or return;
+    my $t = $m{$u}{$to}   or return;
+
+    my %tag = (
+	d => "dst",
+	g => "git",
+	m => "mod",
+	);
+    my $F = $tag{$from};
+    my $T = $tag{$to};
+
+    my $ff = "$f->{dir}/$u";
+    my $tf = "$t->{dir}/$u";
+
+    if ($opt_c) {
+	unlink $ff;
+	system "cp", "-fp", $tf, $ff;
+	}
+
+    open my $fh, "-|", "diff -w $ff $tf";
+    while (<$fh>) {
+	s/^</$F </;
+	s/^>/$T >/;
+	print;
+	}
+    close $fh;
+    } # extdiff
